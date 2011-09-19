c666a6e3b5925d41eed4b8cd031403c72517697b
Claudio A <vecna@delirandom.net>
2011-09-19 18:40:29 +0200

    fixed iusse and stabilized the code. completely working steg/desteg, but not saving correclty

--- README.txt
+++ README.txt
@@ -5,17 +5,25 @@ a simple index for this simple documentation:
 ... 0 .... INTRO .................................. what's and why
 ... 1 .... PHILOSOPHICAL GOAL ..................... what's and why
 ... 2 .... TECHNICAL GOAL ......................... usage, integration 
-... 3 .... ABSTRACT GOAL .......................... vaporware
-... 4 .... PROJECT MAINTENANCE .................... license, git, author
+... 3 .... PROJECT MAINTENANCE .................... license, git, author
 
 0) what's a steganography library written in javascript ?
 
-steganography is explained on wikipedia: https://secure.wikimedia.org/wikipedia/en/wiki/Steganography (not really accurate, btw)
-RabbiSteg is a javascript library to provide an integration/extension of the browser capability, instead the production of a single software implementing a steganographic tool, like the various collected in http://members.cox.net/ebmmd/stego/stego.html
+steganography is explained on wikipedia: 
+https://secure.wikimedia.org/wikipedia/en/wiki/Steganography (not really accurate, btw)
+RabbiSteg is a javascript library to provide an integration/extension of the browser capability, 
+instead the production of a single software implementing a steganographic tool, like the various 
+collected in http://members.cox.net/ebmmd/stego/stego.html
 
 1) philosophical goal
 
-anonymity community, security integralists, crypto rebels: yours is an idea. your idea will not be stopped, and in the world we're facing, our effort are commonly misunderstand. Rabbi, one of the first man contributing in this goal, recenlty has passed out. Only the flash is mortal, not the ideas, neither the technology. This software are is to him dedicated, in him memory and by remind for every other contributor in anonymous technology: you are contributing in a common, shared, permanent knowledge, you will not be forgotten. You share with us a paranoid approach to the technology, but instead of succumb, you're creating a solution. this effort remain forever.
+anonymity community, security integralists, crypto rebels: yours is an idea. your idea will not 
+be stopped, and in the world we're facing, our effort are commonly misunderstand. Rabbi, one of 
+the first man contributing in this goal, recenlty has passed out. Only the flash is mortal, not 
+the ideas, neither the technology. This software are is to him dedicated, in him memory and by 
+remind for every other contributor in anonymous technology: you are contributing in a common, 
+shared, permanent knowledge, you will not be forgotten. You share with us a paranoid approach 
+to the technology, but instead of succumb, you're creating a solution. this effort remain forever.
 
 http://boingboing.net/2011/07/04/rip-len-sassaman-cyp.html
 http://www.cso.com.au/article/392338/young_cryptographer_ends_own_life/
@@ -23,19 +31,33 @@ https://secure.wikimedia.org/wikipedia/en/wiki/Len_Sassaman
 
 2) technical goal
 
-provide a steganography library, easily integrable in every website. Will be in example be integrated in a wordpress module, providing for every user few botton
+provide a steganography library, easily integrable in every website. Will be in example be 
+integrated in a wordpress module, providing for every user few botton permitting to do stegano
+update/download etc...
 
-3) abstract goal
+use the rabbiusage.html as example page implementing Rabbisteg class.
 
+2) technical details
 
+with Canvas object manipoulation, every Image should be accessed as a matrix with four 8-bit
+components for every pixel in the image. Using a simple LSB substitution, this first release
+perform the hiding of an image inside the least 2 significative bits in every 8-bit container.
 
-4) project mantenance
+PNG is the format that will be supported for save image, because the non-downgrade is required
+in order to keep the change.
 
-The license of RabbiSteg is GPL v.3, and should be obtained in http://github.com/vecna/rabbisteg
-The license of the file canvas2image.js, taken by http://www.nihilogic.dk/labs/canvas2image/canvas2image.js, is MIT license
-The license of base64.js, taken by http://www.nihilogic.dk/labs/canvas2image/base64.js, is unlicensed free license.
+3) project mantenance
 
-Actually rabbisteg has been developed only by Claudio Agosti - vecna@delirandom.net, and this software is used in the
-Winston Smith Project's server: http://www.winstonsmith.org/rabbisteg
-at the page http://www.delirandom.net/rabbisteg you should found all blog posts relative to Rabbisteg history, develop and update.
+The license of RabbiSteg is GPL v.3:
+    and should be obtained in http://github.com/vecna/rabbisteg
 
+The license of the file canvas2image.js: 
+    taken by http://www.nihilogic.dk/labs/canvas2image/canvas2image.js, is MIT license
+
+The license of base64.js:
+    taken by http://www.nihilogic.dk/labs/canvas2image/base64.js, is unlicensed free license.
+
+Actually rabbisteg has been developed only by Claudio Agosti - vecna@delirandom.net, and this 
+software is used in the Winston Smith Project's server: http://www.winstonsmith.org/rabbisteg
+at the page http://www.delirandom.net/rabbisteg you should found all blog posts relative to 
+Rabbisteg history, develop and update.
--- rabbisteg.js
+++ rabbisteg.js
@@ -21,56 +21,59 @@ function rabbiSteg()
 
     this.init = function ( typeString, amountOfInject ) 
     {
-        var availType = new Array( "secret", "cover", "extract" );
+        var availType = new Array( "secret", "cover", "extract" );//tipo validi
 
-        for( var i = 0; i < availType.length; i++)
+        for( var i = 0; i < availType.length; i++)//per tutti i tipi
         {
-            if(typeString.match(availType[i]))
-                this.type = typeString;
+            if(typeString.match(availType[i]))//se il tipo passato come argomento è uguale all'i-esimo tipo valido
+                this.type = typeString;//imposto il tipo
         }
 
-        if(this.type == '')
-            return "invalid type of usage sets in rabbiSteg: check the first args";
+        if(this.type == '')//se non ho imposto nessun tipo
+            return "invalid type of usage sets in rabbiSteg: check the first args";//errore
 
         /* generate the bitmasks - at the moment, only 2 is supported */
 	    this.mask = new Array( 3, 12, 48, 192, 15, 240, 3840, 61440 );
         /* end of tmp debug mode */
         
-        return true;
+        return true;//tutto ok
     }
 
     /* event driven function: when the image is load, read the canvas */
     this.loadImage = function( imageElem )
     {
         /* in the initialization method, import the image and grab them as canvas and pixmap */
-        if (!imageElem.src.match("image/*")) 
-            return "This image don't seem to be an Image file";
-
-		this.cnvs = document.createElement("canvas"); 
-        this.cnvs.width = imageElem.width;
-        this.cnvs.height = imageElem.height;
+        if (!imageElem.src.match("image/*"))//se l'elemento non rappresenta un'immagine
+            return "This image don't seem to be an Image file";//errore
 
         /* this cause to inheriet original width and height */
-        this.src = imageElem.src;
-        this.imgRef = imageElem;
+        this.src = imageElem.src;//ottengo i dati dell'immagine
 
         return "";
     }
 
     /* this is splitted from the loadImage function, because sometime happen width and height are not
      * updated with the loaded img */
-    this.imageSetup = function()
+    this.imageSetup = function( stringId )
     {
-		this.ctx = this.cnvs.getContext('2d');
+        /* saving the reference image, just in case */
+        this.imgRef = document.getElementById(stringId);
 
-        this.ctx.drawImage(this.imgRef, 0, 0); 
-		this.pixImg = this.ctx.getImageData(0, 0, this.imgRef.width, this.imgRef.height);
+		this.cnvs = document.createElement("canvas"); //creo un nuovo canvas
+        this.cnvs.width = this.imgRef.width;//imposto larghezza
+        this.cnvs.height = this.imgRef.height;//e altezza
 
-        this.availPixNumber = (this.imgRef.width * this.imgRef.height * 4);
-        if(this.availPixNumber == 0)
-            return "unable to acquire correclty image with role " + this.type;
+		this.ctx = this.cnvs.getContext('2d');//ottengo il contesto del canvas per poter operare sui dati dell'immagine
 
-        return true;
+        this.ctx.drawImage(this.imgRef, 0, 0);//disegno l'immagine nel canvas di appoggio
+
+		this.pixImg = this.ctx.getImageData(0, 0, this.imgRef.width, this.imgRef.height);//ottengo i pixel dell'immagine
+
+        this.availPixNumber = (this.imgRef.width * this.imgRef.height * 4);//calcolo il numero di pixel che posso nascondere
+        if(this.availPixNumber == 0)//se il numero è 0 l'immagine probabilmente non è valida
+            return "unable to acquire correclty image with role " + this.type;//errore
+
+        return true;//tutto ok
     }
 
     this.prologue = function( required )
@@ -90,7 +93,9 @@ function rabbiSteg()
         /* if bitAbuse is 2, the avail pixed need to be almost (8 / 2) = 4 times gretar */
         var timeGreat = (8 / bitAbuse);
 
+        /*
         alert( this.availPixNumber + ' ' + injectable.availPixNumber + ' ' + bitAbuse + ' ' + (8 /  bitAbuse )  );
+         */
 
         if( (this.availPixNumber) <= (injectable.availPixNumber * (8 / bitAbuse) ) )
             return "Invalid: the cover image must be "+ timeGreat  +" greater than the secret image";
@@ -114,33 +119,6 @@ function rabbiSteg()
         spid.innerHTML += "{type: " + this.type + " width: " + this.imgRef.width + " height: " + this.imgRef.height + "} ";
     }
 
-    this.injectNum = function(sourceVal, toInjectNumber, ndxPos)
-    {
-        if(toInjectNumber > 65535) {
-            alert('Image too large to be hidden!');
-        }
-
-        var usableIndex = (ndxPos + 4)
-        /* remind: can't ndxPos += 4 because in >> (ndxPos * 4) need to be 0..3 */
-        var andValue = (toInjectNumber & this.mask[usableIndex]);
-        var leastSign = (andValue >> (ndxPos * 4));
-        sourceVal = ((sourceVal >> 4) << 4) + leastSign;
-
-        return sourceVal;
-    }
-
-    this.extractNum = function(stegoV1, stegoV2, stegoV3, stegoV4)
-    {
-        var retVal = 0;
-
-        retVal += (stegoV1 & this.mask[4]);
-        retVal += ((stegoV2 & this.mask[4]) << 4);
-        retVal += ((stegoV3 & this.mask[4]) << 8);
-        retVal += ((stegoV4 & this.mask[4]) << 12);
-
-        return retVal;
-    }
-
     this.extractValue = function(stegoV1, stegoV2, stegoV3, stegoV4)
     {
         var retVal = 0;
@@ -155,24 +133,57 @@ function rabbiSteg()
 
     this.injectSeq = function( targetVal, start_ndx)
     {
-        for(var i = 0; i < 4; i++)
-            this.pixImg.data[start_ndx + i] = this.injectNum(this.pixImg.data[start_ndx + 0], targetVal, i);
+
+        var bigp = Math.floor(targetVal / 256);
+        var carry = (targetVal % 256);
+
+        alert(' inject of ' + targetVal + ' split in ' + bigp + ' & ' + carry);
+        
+        this.pixImg.data[start_ndx + 0] = this.stegoEmbed(this.pixImg.data[start_ndx + 0], bigp, 0);
+        this.pixImg.data[start_ndx + 1] = this.stegoEmbed(this.pixImg.data[start_ndx + 1], bigp, 1);
+        this.pixImg.data[start_ndx + 2] = this.stegoEmbed(this.pixImg.data[start_ndx + 2], bigp, 2);
+        this.pixImg.data[start_ndx + 3] = this.stegoEmbed(this.pixImg.data[start_ndx + 3], bigp, 3);
+
+        start_ndx += 4;
+        
+        this.pixImg.data[start_ndx + 0] = this.stegoEmbed(this.pixImg.data[start_ndx + 0], carry, 0);
+        this.pixImg.data[start_ndx + 1] = this.stegoEmbed(this.pixImg.data[start_ndx + 1], carry, 1);
+        this.pixImg.data[start_ndx + 2] = this.stegoEmbed(this.pixImg.data[start_ndx + 2], carry, 2);
+        this.pixImg.data[start_ndx + 3] = this.stegoEmbed(this.pixImg.data[start_ndx + 3], carry, 3);
     }
 
     /* this function need to be set as callback */
     this.steganography = function( imageCnvsElem, toEmbedS )
     {
-        alert("I'm on it! workin over: " + this.imgRef.width + " & " + this.imgRef.height+ " = " + this.pixImg.data.length);
+        alert("I'm on it! " + this.imgRef.width + " & " + this.imgRef.height+ " = " + this.pixImg.data.length +
+                " adding "+ toEmbedS.imgRef.width + " & " + toEmbedS.imgRef.height + " = " + toEmbedS.pixImg.data.length);
 
         this.prologue ("cover");
 
         var j = 0;
+        var sI=0;
+/* deb */
+    document.getElementById("debug1").innerHTML += 
+                          "first {"+this.pixImg.data[sI]+"}{"+this.pixImg.data[sI + 1]+"}{"+ 
+                                    this.pixImg.data[sI + 2]+"}{"+ this.pixImg.data[sI+ 3]+"}{ second }{"+
+                                    this.pixImg.data[sI+4]+"}{"+this.pixImg.data[sI + 5]+"}{"+
+                                    this.pixImg.data[sI + 6]+"}{"+this.pixImg.data[sI + 7]+"}**<br>";
+/* / deb */
 
         this.injectSeq(toEmbedS.imgRef.width, j);
-        j += 4;
+        j += 8;
 
         this.injectSeq(toEmbedS.imgRef.height, j);
-        j += 4;
+        j += 8;
+        
+/* / deb */
+    document.getElementById("debug1").innerHTML += 
+                          " after (" + toEmbedS.imgRef.width + ") and ("+ toEmbedS.imgRef.height + ") first {"+
+                                    this.pixImg.data[sI]+"}{"+this.pixImg.data[sI + 1]+"}{"+ 
+                                    this.pixImg.data[sI + 2]+"}{"+ this.pixImg.data[sI+ 3]+"}{ second }{"+
+                                    this.pixImg.data[sI+4]+"}{"+this.pixImg.data[sI + 5]+"}{"+
+                                    this.pixImg.data[sI + 6]+"}{"+this.pixImg.data[sI + 7]+"}**<br>";
+/* / deb */
 
         for(var i = 0 ; i < toEmbedS.pixImg.data.length; i++)
         {
@@ -189,35 +200,68 @@ function rabbiSteg()
             j++;
 
             this.pixImg.data[j] = this.stegoEmbed(this.pixImg.data[j], valueToHide, 3);
+            j++;
         }
 
         imageCnvsElem.width = this.imgRef.width;
         imageCnvsElem.height = this.imgRef.height;
 
         imageCnvsElem.getContext("2d").putImageData(this.pixImg, 0, 0);
-        alert("fine di steganography " + this.pixImg.height + " & " + this.pixImg.width);
+
+        /*
+        alert("end of steganography " + this.pixImg.height + " & " + this.pixImg.width);
+         */
     }
 
     this.extractStegano = function( cnvsEdst )
     {
         var sI = 0;
 
-        this.prologue ("cover");
+        this.prologue ("cover");//controllo che l'immagine si una cover
 
-		var destCtx = cnvsEdst.getContext('2d');
+		var destCtx = cnvsEdst.getContext('2d');//ottengo il contesto del canvas di destinazione
 
-        cnvsEdst.width = this.extractNum(this.pixImg.data[sI], this.pixImg.data[sI + 1], 
-                                    this.pixImg.data[sI + 2], this.pixImg.data[sI+ 3]);
+        cnvsEdst.width=this.pixImg.width;
+        cnvsEdst.height=this.pixImg.height;
+
+        var aCanvasCtx = cnvsEdst.getContext('2d');
+        cnvsEdst.pixImg = aCanvasCtx.createImageData(cnvsEdst.width, cnvsEdst.height);
+
+        document.getElementById("debug2").innerHTML += 
+                    " extract from {"+ this.pixImg.data[sI]+"}{"+this.pixImg.data[sI + 1]+"}{"+ 
+                                       this.pixImg.data[sI + 2]+"}{"+ this.pixImg.data[sI+ 3]+"}{ & }{"+
+                                       this.pixImg.data[sI+4]+"}{"+this.pixImg.data[sI + 5]+"}{"+
+                                       this.pixImg.data[sI + 6]+"}{"+this.pixImg.data[sI + 7]+"};";
+		   
+		//calcolo larghezza dell'immagine nascosta
+        var bigW = this.extractValue(this.pixImg.data[sI], this.pixImg.data[sI + 1], 
+                                     this.pixImg.data[sI + 2], this.pixImg.data[sI + 3]);
         sI += 4;
-        cnvsEdst.height = this.extractNum(this.pixImg.data[sI], this.pixImg.data[sI + 1], 
-                                    this.pixImg.data[sI + 2], this.pixImg.data[sI + 3]);
+        var carryW = this.extractValue(this.pixImg.data[sI], this.pixImg.data[sI + 1], 
+                                     this.pixImg.data[sI + 2], this.pixImg.data[sI + 3]);
         sI += 4;
+        
+        //calcolo altezza dell'immagine nascosta
+        var bigH = this.extractValue(this.pixImg.data[sI], this.pixImg.data[sI + 1], 
+                                     this.pixImg.data[sI + 2], this.pixImg.data[sI + 3]);
+        sI += 4;
+        var carryH = this.extractValue(this.pixImg.data[sI], this.pixImg.data[sI + 1], 
+                                     this.pixImg.data[sI + 2], this.pixImg.data[sI + 3]);
+        sI += 4;
+
+/*
+        final computing 
+*/
+        cnvsEdst.width = (bigW * 256) + carryW;
+        cnvsEdst.height = (bigH * 256) + carryH;
+
+        alert('extracted H ' + cnvsEdst.height + ' from ' + bigH + 'x 256 & ' + carryH + 'extracted W ' + cnvsEdst.width + ' from ' + bigW + 'x 256 & ' + carryW);
 
         /* will be strage that a cover image is used, in the same session as container and
          * dest. I belive that software interfaces need to be less limited where possibile,
          * because the next generations will perceive the Intertubes with eyes none ever had */
-
-        alert('extracted width/height: ' + cnvsEdst.width, + ' e ' + cnvsEdst.height);
+ 
+//        alert('extracted width/height: ' + cnvsEdst.width + ' e ' + cnvsEdst.height);
 
         var aCanvasCtx = cnvsEdst.getContext('2d');
         cnvsEdst.pixImg = aCanvasCtx.createImageData(cnvsEdst.width, cnvsEdst.height);
